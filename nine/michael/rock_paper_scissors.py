e2cd418442489e3fdc6a67aec10375a38281e3a3
Obi Michael <maxclass247@gmail.com>
2022-01-12 16:13:12 +0000

    Assignments

--- nine/michael/rock_paper_scissors.py
+++ nine/michael/rock_paper_scissors.py
@@ -5,10 +5,12 @@ while(count < 3):
     user_input_1 =input("Enter rock, paper or scissors: ")
     user_input_2 = input("Enter rock, paper or scissors: ")
 
-    if user_input_1 == "rock" and user_input_2 == "paper" or user_input_1 == "scissors" and user_input_2 == "rock" or user_input_1 == "paper" and user_input_2 == "scissors":
+    if user_input_1 == "rock" and user_input_2 == "paper" or user_input_1 == "scissors" \
+            and user_input_2 == "rock" or user_input_1 == "paper" and user_input_2 == "scissors":
         print("user two wins")
         player_two_point +=1
-    elif user_input_1 == "rock" and user_input_2 == "scissors" or user_input_1 == "rock" and user_input_2 == "paper" or user_input_1 == "scissors" and user_input_2 == "paper":
+    elif user_input_1 == "rock" and user_input_2 == "scissors" or user_input_1 == "rock" and \
+            user_input_2 == "paper" or user_input_1 == "scissors" and user_input_2 == "paper":
         print("user one wins")
         player_one_point +=1
     else:
